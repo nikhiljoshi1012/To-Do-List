3494017051558c75bf0a29a075755abe0bb275be
nikhiljoshi1012 <nikhiljoshi1012@gmail.com>
2023-11-05 11:33:41 +0530

    Fixes
    Fixed some errors in html and JavaScript
    also add transitioning background

--- TO-DO-LIST_J.js
+++ TO-DO-LIST_J.js
@@ -1,37 +1,36 @@
-//Java script to store new tasks
-const task=[];
+// JavaScript to store new tasks
+const task = [];
 
-//Function to add new tasks
-function addTask()
-{
-    const taskInput= document.getElementById("taskinput").value;
-    
-    if(taskInput===""){
-    alert("Please Enter a task");
-    return;
+// Function to add new tasks
+function addTask() {
+    const taskInput = document.getElementById("taskinput").value;
 
+    if (taskInput === "") {
+        alert("Please Enter a task");
+        return;
+    }
+
+    // Convert the task to tasks array
+    task.push(taskInput);
+
+    // Clear input
+    document.getElementById("taskinput").value = "";
+
+    // Update the task list
+    updateTaskList();
 }
-//Convert  the task to  tasks array
-task.push(taskInput);
-}
 
-//Update DOM
-updateTaskList();
-
-//clear input
-document.getElementById("taskinput").value="";
-
-//function Remove Task
-function removetask(index){
-    //remove the task from the tasks array
-    task.splice(index,1);
+// Function to remove a task
+function removetask(index) {
+    // Remove the task from the tasks array
+    task.splice(index, 1);
 
     updateTaskList();
 }
 
-function clearall()
-{
-    task.length=0;
+// Function to clear all tasks
+function clearall() {
+    task.length = 0;
 
     updateTaskList();
 }
@@ -40,21 +39,18 @@ function clearall()
 function updateTaskList() {
     const taskList = document.getElementById("taskList");
     taskList.innerHTML = "";
-  
+
     for (let i = 0; i < task.length; i++) {
-      const listItem = document.createElement("li");
-      const taskText = document.createTextNode(task[i]);
-      const removeButton = document.createElement("button");
-      removeButton.textContent = "Remove";
-      removeButton.className = "remove-button";
-      removeButton.onclick = function() {
-        removetask(i);
-      };
-      listItem.appendChild(taskText);
-      listItem.appendChild(removeButton);
-      taskList.appendChild(listItem);
+        const listItem = document.createElement("li");
+        const taskText = document.createTextNode(task[i]);
+        const removeButton = document.createElement("button");
+        removeButton.textContent = "Remove";
+        removeButton.className = "remove-button";
+        removeButton.onclick = function () {
+            removetask(i);
+        };
+        listItem.appendChild(taskText);
+        listItem.appendChild(removeButton);
+        taskList.appendChild(listItem);
     }
-  }
-  
-
-
+}
